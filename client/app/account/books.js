1af65ebf852b1583f097192c3127a47b19e052e5
DaniaHamdan <dandonzh@gmail.com>
2017-02-20 14:21:56 +0200

    book

--- client/app/account/books.js
+++ client/app/account/books.js
@@ -2,12 +2,13 @@ angular.module('addbook' , [])
 
 .controller('BookController', function ($scope , $window , $location , book) {
   $scope.book = {};
+  $scope.buy={"id":false};
 
-  if($location.path() === '/books/add'){
-  if(!$window.localStorage.getItem("user.type")) {
-        $location.path('/');
-      } 
-  }
+  // if($location.path() === '/books/add'){
+  // if(!$window.localStorage.getItem("user.type")) {
+  //       $location.path('/');
+  //     } 
+  // }
   // function add book just for admin 
   $scope.addbook = function () {
   	book.addbook($scope.book)
@@ -19,7 +20,23 @@ angular.module('addbook' , [])
         console.log(error);
       });
   }
+  // function to add book to the cart
+  $scope.Buy=function(title,price){
+    $scope.buy['id']=true
+    $scope.buy[title]={title:price}
+     console.log($scope.buy,price)
+    }
+    // console.log(price,array)
+  $scope.cancel=function(title){
+    delete $scope.buy[title]
+    console.log($scope.buy)
 
+  }
+  // $scope.result=function(){
+  //   $scope.bookBuy=$scope.buy
+  //   $scope.book
+
+  // }
   // function show book for user and admin
   $scope.showBook = function () {
   	book.showbook($scope.book).then(function(data) {
