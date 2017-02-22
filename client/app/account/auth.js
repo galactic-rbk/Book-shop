727bf1a8543b746846cae498dd5d461648f8fc0a
cozmoc <cozmocozi@gmail.com>
2017-02-22 14:35:49 +0200

    added uploading and downloading files through /upload and /links

--- client/app/account/auth.js
+++ client/app/account/auth.js
@@ -33,9 +33,9 @@ angular.module('book.auth', [])
       if(!userFlag && !passFlag){
         $scope.msg = "Wrong input for user or Password  "
       } else if(!userFlag){
-        $scope.msg = "please inter your username"
+        $scope.msg = "please enter your username"
       } else if (!passFlag){
-        $scope.msg = "please inter your password"
+        $scope.msg = "please enter your password"
       }
     }
   }
@@ -58,9 +58,9 @@ angular.module('book.auth', [])
       if(!userFlag && !passFlag){
        $scope.msg = "Wrong input for user or Password"
      } else if(!userFlag){
-      $scope.msg = "please inter all fild"
+      $scope.msg = "please enter all fields"
     } else if (!passFlag){
-      $scope.msg = "please inter all fild"
+      $scope.msg = "please enter all fields"
     }
   }
 }
--- client/app/account/books.js
+++ client/app/account/books.js
@@ -3,12 +3,15 @@ angular.module('addbook' , [])
 .controller('BookController', function ($scope , $window , $location , book) {
   $scope.book = {};
 
-  if($location.path() === '/books/add'){
+  if($location.path() === '/books/add' || $location.path() === '/links'){
   if(!$window.localStorage.getItem("user.type")) {
         $location.path('/');
       } 
   }
+
   // function add book just for admin 
+  $scope.book.type="book"
+  $scope.book.approved='true'
   $scope.addbook = function () {
   	book.addbook($scope.book)
   	.then(function (Book) {
@@ -19,13 +22,77 @@ angular.module('addbook' , [])
         console.log(error);
       });
   }
-
-  // function show book for user and admin
-  $scope.showBook = function () {
-  	book.showbook($scope.book).then(function(data) {
-		$scope.book = data;
-	});
+  $scope.updatebook = function (bk) {
+    console.log(bk)
+    book.updatebook(bk)
+    .then(function (Book) {
+        console.log(Book)
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+   $scope.showBook = function () {
+    book.showbook($scope.book).then(function(data) {
+    $scope.book = data;
+  });
   }
 
-  });
+  /// turn image into buffer
+  $scope.click=false
+  $scope.imagetext=""
+  $scope.reader=new FileReader();
+  $scope.loading=false;
+  $scope.previewFile=function(){
+    $scope.loading=true;
+    $scope.$apply();
+    if ($scope.image_url===undefined || Object.keys($scope.image_url).length){
+      $scope.book.image_url=$scope.imagetext
+      $scope.$apply()
+    }else{
+      $scope.reader.readAsDataURL($scope.image_url); //reads the data as a URL
+      setTimeout(function(){
+        $scope.book.image_url=$scope.reader.result;
+        $scope.$apply()
+      },500);
+    }
+    $scope.loading=false;
+  }
+
+  //upload book
+ 
+  $scope.file={}
+  $scope.filetext="";
+  $scope.readfile=new FileReader();
+  $scope.prosFile=function(){
+    $scope.book.approved='false'
+    $scope.loading=true;
+    $scope.$apply()
+    if ($scope.filetext!==""){
+      $scope.book.type="link"
+      //$scope.book.title="link"
+      $scope.book.link=$scope.filetext
+    }else{
+      $scope.readfile.readAsDataURL($scope.file); //reads the data as a URL
+      setTimeout(function(){
+        $scope.book.link=$scope.readfile.result;
+        $scope.book.type="link"
+        $scope.$apply()
+        //console.log($scope.readfile.result)
+        },1000);
+      }
+      $scope.loading=false;
+    }
+ 
+  // if($location.path() === "/upload"){
+  //   alert('hey')
+  // }
+
+$scope.booktab=function(link){
+  window.open(link,"_blank")
+}
+
+});
+
+
 
